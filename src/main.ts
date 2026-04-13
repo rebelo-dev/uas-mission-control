42d6cc4f7b0888860ecbf7c755bc1a743bec8a30
rebelo-dev <joao.rebelo.dev@gmail.com>
2026-04-13 23:58:12 +0100

    requests from another port are now allowed
    requests from another port are now allowed
    http requests in insomnia collection moved to new folder Insomnia/postman

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.enableCors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
