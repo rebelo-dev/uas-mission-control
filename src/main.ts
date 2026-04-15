068ef5df8e25f4011adb25e8583864b72c6f53f1
rebelo-dev <joao.rebelo.dev@gmail.com>
2026-04-15 21:38:03 +0100

    cors test for prod

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,15 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (origin.includes("vercel.app") || origin === "http://localhost:5173") {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   });
   await app.listen(process.env.PORT ?? 3000);
