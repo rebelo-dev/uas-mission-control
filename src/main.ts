3b88e924b88d9a5d9d2c032da429f0bdbe755bd4
rebelo-dev <joao.rebelo.dev@gmail.com>
2026-05-20 02:32:14 +0100

    fix:cors and node update

--- src/main.ts
+++ src/main.ts
@@ -14,11 +14,27 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  /*app.enableCors({
+     origin: (origin, callback) => {
+       if (!origin) return callback(null, true);
+ 
+       if (origin.includes("vercel.app") || origin === "http://localhost:5173") {
+         return callback(null, true);
+       }
+ 
+       return callback(new Error("Not allowed by CORS"));
+     },
+     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+   });*/
   app.enableCors({
     origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
+      const currentOrigin = origin || "";
 
-      if (origin.includes("vercel.app") || origin === "http://localhost:5173") {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (currentOrigin.includes("vercel.app") || currentOrigin === "http://localhost:5173") {
         return callback(null, true);
       }
 
